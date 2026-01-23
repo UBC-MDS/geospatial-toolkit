020b5a9fed86ab5481d0d3ef129b15e873a30c45
sjbalagit <shrabantibala1502@gmail.com>
2026-01-23 11:47:31 -0800

    added TypeError to handle non numeric inputs

--- src/geospatial_toolkit/latlong.py
+++ src/geospatial_toolkit/latlong.py
@@ -50,7 +50,7 @@ def standardize_latlong(lat, lon):
         """
         try:
             return float(coord)
-        except (ValueError):
+        except (ValueError, TypeError):
             pass
 
         if isinstance(coord, str):
